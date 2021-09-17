3e46a504a8cf6c8e8d65cfe20708afab1f456979
anuj-algoworks <anuj.kumar@algoworks.com>
2021-09-17 09:32:57 +0530

    Added executers

---
+++ app/src/main/java/com/android/executer/Main.java
@@ -0,0 +1,145 @@
+package com.android.executer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AtomicBoolean processing = new AtomicBoolean(true);
+
+        new Executor.Builder()
+                .add(() -> {
+                    System.out.println("TASK 1 Start");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("TASK 1 Complete");
+                })
+                .add(() -> {
+                    System.out.println("TASK 2 Start");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("TASK 2 Complete");
+                })
+                .add(() -> {
+                    System.out.println("TASK 3 Start");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("TASK 3 Complete");
+                })
+                .callback(() -> {
+                    System.out.println("All TASK COMPLETED");
+                    processing.set(false);
+                })
+                .build()
+                .execute();
+
+        while (processing.get()) {
+            // program runs continuously
+        }
+        System.out.println("Program Terminates");
+    }
+
+    public static class Executor extends Thread {
+        private final ConcurrentLinkedQueue<Worker> workers;
+        private final Callback callback;
+        private final CountDownLatch latch;
+
+        private Executor(List<Runnable> tasks, Callback callback) {
+            super();
+            this.callback = callback;
+            workers = new ConcurrentLinkedQueue<>();
+            latch = new CountDownLatch(tasks.size());
+
+            for (Runnable task : tasks) {
+                workers.add(new Worker(task, latch));
+            }
+        }
+
+        public void execute() {
+            start();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                Worker worker = workers.poll();
+                if (worker == null) {
+                    break;
+                }
+                worker.start();
+            }
+
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (callback != null) {
+                callback.onComplete();
+            }
+        }
+
+        public interface Callback {
+            void onComplete();
+        }
+
+        public static class Builder {
+            private final List<Runnable> tasks = new ArrayList<>();
+            private Callback callback;
+
+            public Builder add(Runnable task) {
+                tasks.add(task);
+                return this;
+            }
+
+            public Builder callback(Callback callback) {
+                this.callback = callback;
+                return this;
+            }
+
+            public Executor build() {
+                return new Executor(tasks, callback);
+            }
+        }
+    }
+
+    public static class Worker implements Runnable {
+
+        private final AtomicBoolean started;
+        private final Runnable task;
+        private final Thread thread;
+        private final CountDownLatch latch;
+
+        public Worker(Runnable task, CountDownLatch latch) {
+            this.latch = latch;
+            this.task = task;
+            started = new AtomicBoolean(false);
+            thread = new Thread(this);
+        }
+
+        public void start() {
+            if (!started.getAndSet(true)) {
+                thread.start();
+            }
+        }
+
+        @Override
+        public void run() {
+            task.run();
+            latch.countDown();
+        }
+    }
+}
